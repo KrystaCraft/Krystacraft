1488744c625053276e586e316b977f09d4c0231d
Shadow2004 <chrispratt1985@gmail.com>
2013-11-10 12:35:52 +0000

    Biome specific generation code finished

--- common/krystacraft/wep/worldgen/WorldGenManager.java
+++ common/krystacraft/wep/worldgen/WorldGenManager.java
@@ -26,44 +26,127 @@ public class WorldGenManager implements IWorldGenerator {
         
         switch(b.biomeName){
         case "Ocean": generateOcean(world, random, chunkX, chunkZ);
-        case "Plains": generateOcean(world, random, chunkX, chunkZ);
-        case "Desert": generateOcean(world, random, chunkX, chunkZ);
-        case "Extreme Hills": generateOcean(world, random, chunkX, chunkZ);
-        case "Forest": generateOcean(world, random, chunkX, chunkZ);
-        case "Taiga": generateOcean(world, random, chunkX, chunkZ);
-        case "Swampland": generateOcean(world, random, chunkX, chunkZ);
-        case "River": generateOcean(world, random, chunkX, chunkZ);
-        case "Hell": generateOcean(world, random, chunkX, chunkZ);
-        case "Sky": generateOcean(world, random, chunkX, chunkZ);
-        case "FrozenOcean": generateOcean(world, random, chunkX, chunkZ);
-        case "FrozenRiver": generateOcean(world, random, chunkX, chunkZ);
-        case "Ice Plains": generateOcean(world, random, chunkX, chunkZ);
-        case "Ice Mountains": generateOcean(world, random, chunkX, chunkZ);
-        case "MushroomIsland": generateOcean(world, random, chunkX, chunkZ);
-        case "MushroomIslandShore": generateOcean(world, random, chunkX, chunkZ);
-        case "Beach": generateOcean(world, random, chunkX, chunkZ);
-        case "DesertHills": generateOcean(world, random, chunkX, chunkZ);
-        case "ForestHills": generateOcean(world, random, chunkX, chunkZ);
-        case "TaigaHills": generateOcean(world, random, chunkX, chunkZ);
-        case "Extreme Hills Edge": generateOcean(world, random, chunkX, chunkZ);
-        case "Hungle": generateOcean(world, random, chunkX, chunkZ);
-        case "JungleHills": generateOcean(world, random, chunkX, chunkZ);
+        case "Plains": generatePlains(world, random, chunkX, chunkZ);
+        case "Desert": generateDesert(world, random, chunkX, chunkZ);
+        case "Extreme Hills": generateExtremeHills(world, random, chunkX, chunkZ);
+        case "Forest": generateForest(world, random, chunkX, chunkZ);
+        case "Taiga": generateTaiga(world, random, chunkX, chunkZ);
+        case "Swampland": generateSwampland(world, random, chunkX, chunkZ);
+        case "River": generateRiver(world, random, chunkX, chunkZ);
+        case "Hell": generateHell(world, random, chunkX, chunkZ);
+        case "Sky": generateSky(world, random, chunkX, chunkZ);
+        case "FrozenOcean": generateFrozenOcean(world, random, chunkX, chunkZ);
+        case "FrozenRiver": generateFrozenRiver(world, random, chunkX, chunkZ);
+        case "Ice Plains": generateIcePlains(world, random, chunkX, chunkZ);
+        case "Ice Mountains": generateIceMountains(world, random, chunkX, chunkZ);
+        case "MushroomIsland": generateMushroomIsland(world, random, chunkX, chunkZ);
+        case "MushroomIslandShore": generateMushroomIslandShore(world, random, chunkX, chunkZ);
+        case "Beach": generateBeach(world, random, chunkX, chunkZ);
+        case "DesertHills": generateDesertHills(world, random, chunkX, chunkZ);
+        case "ForestHills": generateForestHills(world, random, chunkX, chunkZ);
+        case "TaigaHills": generateTaigaHills(world, random, chunkX, chunkZ);
+        case "Extreme Hills Edge": generateExtremeHillsEdge(world, random, chunkX, chunkZ);
+        case "Jungle": generateJungle(world, random, chunkX, chunkZ);
+        case "JungleHills": generateJungleHills(world, random, chunkX, chunkZ);
         }
         
-		switch(world.provider.dimensionId){
-		case -1: generateNether(world, random, chunkX, chunkZ);
-		case 0: generateSurface(world, random, chunkX, chunkZ);
-		case 1: generateEnd(world, random, chunkX, chunkZ);
-		}
-
+        //Generate OverWorld items
+        if (world.provider.dimensionId == 0){
+        	generateSurface(world, random, chunkX, chunkZ);
+        }
 	}
 	
 	private void generateOcean(World world, Random random, int x, int z){
 		
 	}
-
-	private void generateEnd(World world, Random random, int x, int z) {
-
+	
+	private void generatePlains(World world, Random random, int x, int z){
+		oreGen.generateOre(Blocks.krystaOre, Block.stone, world , random, x, z, 160, 4, 30);
+    	oreGen.generateOre(Blocks.sodaliteOre, Block.stone, world, random, x, z, 160, 4, 50);
+	}
+	
+	private void generateDesert(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateExtremeHills(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateForest(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateTaiga(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateSwampland(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateRiver(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateHell(World world, Random random, int x, int z){
+		oreGen.generateOre(Blocks.krystaOre, Block.netherrack, world , random, x, z, 160, 4, 30);
+	}
+	
+	private void generateSky(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateFrozenOcean(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateFrozenRiver(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateIcePlains(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateIceMountains(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateMushroomIsland(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateMushroomIslandShore(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateBeach(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateDesertHills(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateForestHills(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateTaigaHills(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateExtremeHillsEdge(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateJungle(World world, Random random, int x, int z){
+		
+	}
+	
+	private void generateJungleHills(World world, Random random, int x, int z){
+		
 	}
 
 	private void generateSurface(World world, Random random, int x, int z) {
@@ -72,16 +155,5 @@ public class WorldGenManager implements IWorldGenerator {
 		int Ycoord1 = random.nextInt(100); //how high it generates
 		int Zcoord1 = z + random.nextInt(16); //where in chunk it generates
 		worldGen.generate(world, random, Xcoord1, Ycoord1, Zcoord1);
-		
-		//Generate Ores
-		BiomeGenBase b = world.getBiomeGenForCoords(x, z);
-        if(b.biomeName.equals("Plains")) {
-        	oreGen.generateOre(Blocks.krystaOre, Block.stone, world , random, x, z, 160, 4, 30);
-        	oreGen.generateOre(Blocks.sodaliteOre, Block.stone, world, random, x, z, 160, 4, 50);
-        }
-	}
-
-	private void generateNether(World world, Random random, int x, int z) {
-		oreGen.generateOre(Blocks.krystaOre, Block.netherrack, world , random, x, z, 160, 4, 30);
 	}
 }
