3e2b47b783e4bd056b817c2dae2a611838e9a59b
Shadow2004 <chrispratt1985@gmail.com>
2013-11-11 18:30:42 +0000

    Started to work on blocks dropping multiple items

--- common/krystacraft/blocks/BlockMalachiteOre.java
+++ common/krystacraft/blocks/BlockMalachiteOre.java
@@ -32,15 +32,18 @@ public class BlockMalachiteOre extends Block {
 		blockIcon = icon.registerIcon(References.MOD_ID.toLowerCase() + ":block" + Strings.BlockMalachiteOre_unlocalizedName);
 	}
 	
+	@Override
 	public int idDropped(int metadata, Random random, int fortune){
 		return Items.krystaDust.itemID;
 	}
 	
+	@Override
 	public int quantityDropped(Random par1Random)
     {
         return 2 + par1Random.nextInt(2);
     }
 	
+	@Override
 	public void dropBlockAsItemWithChance(World par1World, int par2, int par3, int par4, int par5, float par6, int par7)
     {
         super.dropBlockAsItemWithChance(par1World, par2, par3, par4, par5, par6, par7);
